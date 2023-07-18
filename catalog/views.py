8fe4b4cefdddd89c732f28407431ae996caebe56
Ann-Ryazanova <zai97@mail.ru>
2023-07-19 01:28:14 +0300

    Homework 20.2

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
+from catalog.models import Product
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product.html'
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    product_list = Product.objects.all()
+
+    context = {
+        'object_list': product_list,
+        'title': 'Продуктовая лавка'
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
@@ -14,4 +28,18 @@ def contacts(request):
         print(f'Имя: {name}, телефон: {phone};\n'
               f'Сообщение: {message}')
 
-    return render(request, 'catalog/contacts.html')
+    context = {
+        'title': 'Контакты'
+    }
+
+    return render(request, 'catalog/contacts.html', context)
+
+
+def product_card(request, pk):
+
+    context = {
+        'object_list': Product.objects.get(pk=pk),
+        'title': 'Страница продукта'
+    }
+
+    return render(request, 'catalog/product.html', context)
