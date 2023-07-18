43de829749bc54c17a6d6cd44f94bce5074d571d
Ann-Ryazanova <zai97@mail.ru>
2023-07-19 01:28:08 +0300

    Homework 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from catalog.views import home, contacts
+
+from catalog.apps import CatalogConfig
+from . import views
+
+# from catalog.views import home, contacts, product_card
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts)
+    path('', views.home),
+    path('contacts/', views.contacts),
+    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_card')
+
+    # <img src="/{{ BASIC_DIR }}/{{media.media}} class="card-img-top">
 ]
