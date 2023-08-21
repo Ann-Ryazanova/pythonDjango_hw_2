52ddd33bce8eb17665c0fdd049e37eb497fa6b0b
Ann-Ryazanova <zai97@mail.ru>
2023-08-21 20:28:46 +0300

    Homework 22.1

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Version
 
 
 @admin.register(Category)
@@ -13,4 +13,10 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name_product', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('name_product', 'description',)
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'number_version', 'current_version',)
+    list_filter = ('product', 'current_version',)
+
 
