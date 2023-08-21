d425f0826258093544377f0097bafebe731398e3
Ann-Ryazanova <zai97@mail.ru>
2023-08-21 20:29:14 +0300

    Homework 22.1

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,7 +7,10 @@ from . import views
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.ProductListView.as_view()),
-    path('contacts/', views.ProductContactView.as_view()),
-    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_card')
+    path('create/', views.ProductCreateView.as_view(), name='create_product'),
+    path('', views.ProductListView.as_view(), name='list'),
+    path('contact/', views.ProductContactView.as_view()),
+    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('edit/<int:pk>/', views.ProductUpdateView.as_view(), name='update_product'),
+    path('delete/<int:pk>/', views.ProductDeleteView.as_view(), name='delete_product'),
 ]
