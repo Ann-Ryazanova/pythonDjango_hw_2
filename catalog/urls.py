b19902c3bebf091e2fa632b79feb7796842f4c8c
Ann-Ryazanova <zai97@mail.ru>
2023-08-24 00:17:58 +0300

    Homework 22.1 (Добавление стилей)

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,7 +9,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('create/', views.ProductCreateView.as_view(), name='create_product'),
     path('', views.ProductListView.as_view(), name='list'),
-    path('contact/', views.ProductContactView.as_view()),
+    path('contact/', views.ProductContactView.as_view(), name='contacts'),
     path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
     path('edit/<int:pk>/', views.ProductUpdateView.as_view(), name='update_product'),
     path('delete/<int:pk>/', views.ProductDeleteView.as_view(), name='delete_product'),
