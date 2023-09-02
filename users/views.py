d18438bcd0e6e183e18716a91de486c4845abba5
Ann-Ryazanova <zai97@mail.ru>
2023-09-02 23:40:28 +0300

    Homework 22.2

---
+++ users/views.py
@@ -0,0 +1,98 @@
+import random
+from django.contrib.auth import login
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy, reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.views import View
+from django.contrib.auth.tokens import default_token_generator as token_generator
+from django.core.mail import EmailMessage, send_mail
+
+from django.views.generic import CreateView, UpdateView
+
+from config import settings
+from users.forms import UserRegisterForm, UserProfileForm
+from users.models import User
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+
+    # верификация
+    def form_valid(self, form):
+        """Отправка ссылки для верификации"""
+
+        user = form.save(commit=False)
+        user.email_verify = False
+        user.save()
+
+        current_site = get_current_site(self.request)
+        mail_subject = 'Activation link has been sent to your email id'
+        message = render_to_string('users/verify_email.html', {
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': token_generator.make_token(user),
+        })
+
+        email = EmailMessage(
+            mail_subject,
+            message,
+            to=[user.email]
+        )
+
+        email.send()
+        return redirect('users:confirm_email')
+
+
+class EmailVerify(View):
+    def get(self, request, uidb64, token):
+        user = self.get_user(uidb64)
+
+        if user is not None and token_generator.check_token(user, token):
+            user.email_verify = True
+            user.save()
+            login(request, user)
+            return redirect('users:confirm_email_done')
+
+        return redirect('users:invalid_veryfi')
+
+    @staticmethod
+    def get_user(uidb64):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = User.objects.get(pk=uid)
+
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        return user
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    success_url = reverse_lazy('users:profile')
+    form_class = UserProfileForm
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+def generate_new_password(request):
+    new_password = new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+
+    send_mail(
+        subject='Вы сменили пароль',
+        message=f'Ваш новый пароль: {new_password}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email]
+    )
+
+    request.user.set_password(new_password)
+    request.user.save()
+
+    return redirect(reverse('catalog:list'))
