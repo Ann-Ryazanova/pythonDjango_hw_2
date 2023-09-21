ef8be4bd9085b73362eb8d82ee5fb4c4ffa06eac
Ann-Ryazanova <zai97@mail.ru>
2023-09-22 00:34:34 +0300

    Homework 23.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page, never_cache
 
 from catalog.apps import CatalogConfig
 from . import views
@@ -6,10 +7,10 @@ from . import views
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('create/', views.ProductCreateView.as_view(), name='create_product'),
+    path('create/', never_cache(views.ProductCreateView.as_view()), name='create_product'),
     path('', views.ProductListView.as_view(), name='list'),
     path('contact/', views.ProductContactView.as_view(), name='contacts'),
-    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
-    path('edit/<int:pk>/', views.ProductUpdateView.as_view(), name='update_product'),
+    path('product/<int:pk>/', cache_page(60)(views.ProductDetailView.as_view()), name='product_detail'),
+    path('edit/<int:pk>/', never_cache(views.ProductUpdateView.as_view()), name='update_product'),
     path('delete/<int:pk>/', views.ProductDeleteView.as_view(), name='delete_product'),
 ]
